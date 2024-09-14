395a2390f094224774f978c5604b23cd7c064be1
Nathaniel Callabresi <n.callabresi@hotmail.com>
2024-09-14 05:38:13 -0400

    Add files via upload

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -2,10 +2,11 @@ import { initializeAuthProxy } from '@propelauth/auth-proxy'
 
 // Replace with your configuration
 await initializeAuthProxy({
-    authUrl: "",
-    integrationApiKey: "",
+    // authUrl: "https://7133856604.propelauthtest.com/",
+    authUrl: "https://auth.homeview.studio/",
+    integrationApiKey: "ad6d9ee78842e20a08652805026306f464f9927ff32867c2170ba66c567de74c607106643686f0713f89e5d14ce5aa1c",
     proxyPort: 8000,
-    urlWhereYourProxyIsRunning: 'http://localhost:8000',
+    urlWhereYourProxyIsRunning: 'https://homeview.studio',
     target: {
         host: 'localhost',
         port: 8501,
