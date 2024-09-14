bcb148a89628af45969bf751be668a554c481425
mcropper14 <45805132+mcropper14@users.noreply.github.com>
2024-09-14 01:12:05 -0400

    Update proxy.mjs

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -2,8 +2,8 @@ import { initializeAuthProxy } from '@propelauth/auth-proxy'
 
 // Replace with your configuration
 await initializeAuthProxy({
-    authUrl: "https://7133856604.propelauthtest.com/",
-    integrationApiKey: "2b9c67e49f090cf533942dde2bff4a2123d7bca6569bb9c2d91df23f34f7683c2e3fec5fc3e7a9b87b5db69a39c5f171",
+    authUrl: "",
+    integrationApiKey: "",
     proxyPort: 8000,
     urlWhereYourProxyIsRunning: 'http://localhost:8000',
     target: {
