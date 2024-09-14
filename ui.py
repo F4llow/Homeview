aa7b23f790a27d01c6e67c27fc927e9aff42b53e
cropthecoder <myracropper32@gmail.com>
2024-09-13 22:37:21 -0400

    ugh auth

--- ui.py
+++ ui.py
@@ -8,6 +8,17 @@ from models import get_list_of_models
 
 from llm import getStreamingChain
 
+from propelauth import auth
+
+user = auth.get_user()
+if user is None:
+    st.error('Unauthorized')
+    st.stop()
+
+with st.sidebar:
+    st.link_button('Account', auth.get_account_url(), use_container_width=True)
+
+st.text("Logged in as " + user.email + " with user ID " + user.user_id)
 
 
 
