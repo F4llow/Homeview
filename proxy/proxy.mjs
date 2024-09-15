c320198d258ec0228740ed50d28a48860fe2084f
mcropper14 <45805132+mcropper14@users.noreply.github.com>
2024-09-14 21:34:28 -0400

    Update proxy.mjs

--- proxy/proxy.mjs
+++ proxy/proxy.mjs
@@ -3,8 +3,8 @@ import { initializeAuthProxy } from '@propelauth/auth-proxy'
 // Replace with your configuration
 await initializeAuthProxy({
     // authUrl: "https://7133856604.propelauthtest.com/",
-    authUrl: "https://auth.homeview.studio/",
-    integrationApiKey: "ad6d9ee78842e20a08652805026306f464f9927ff32867c2170ba66c567de74c607106643686f0713f89e5d14ce5aa1c",
+    authUrl: "",
+    integrationApiKey: "",
     proxyPort: 8000,
     urlWhereYourProxyIsRunning: 'https://homeview.studio',
     target: {
